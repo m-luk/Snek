763ab60c0e2e62399d7a558f818af45f40c72240
Michał Łukaszewicz <mlukaszewicz2@gmail.com>
2019-03-21 17:50:43 +0100

    fixed snek's out of window bounds behaviour next up TODO is "self eating" prohibition and some food generator

--- main.py
+++ main.py
@@ -7,50 +7,63 @@ FPS = 15
 MOVEMENT_STEP = 10
 SQR_SIZE = 10
 
-WHITE = (255,255,255)
-RED = (255,0,0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 CLOCK = pygame.time.Clock()
+
 
 class position:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.last_pos = None
-    def set_cord(self, cord):
-        self.last_pos = (self.x, self.y)
+
+    def set_cord(self, cord, save_last=True):
+        if save_last:
+            self.last_pos = (self.x, self.y)
+
         self.x = cord[0]
         self.y = cord[1]
+
     def get_cord(self):
         return [self.x, self.y]
-    def move_by_vector(self, vec, multi):
+
+    def move_by_vector(self, vec, multi, save_last=True):
+        if save_last:
+            self.last_pos = (self.x, self.y)
+
         self.last_pos = (self.x, self.y)
-        self.x += vec[0]*multi
-        self.y += vec[1]*multi
+        self.x += vec[0] * multi
+        self.y += vec[1] * multi
+
 
 class square(position):
-    def __init__(self, x,y,a, color=None):
+    def __init__(self, x, y, a, color=None):
         position.__init__(self, x, y)
         self.a = a
-        self.area = a**2
+        self.area = a ** 2
         self.color = color
+
     def gett(self):
-        return  pygame.Rect(self.x, self.y, self.a, self.a)
+        return pygame.Rect(self.x, self.y, self.a, self.a)
+
     def __repr__(self):
         return "square at ({},{})".format(self.x, self.y)
 
+
 class snek:
     def __init__(self):
-        assert(WINDOWHEIGHT%SQR_SIZE==0 and WINDOWWIDTH%SQR_SIZE==0)
+        assert (WINDOWHEIGHT % SQR_SIZE == 0 and WINDOWWIDTH % SQR_SIZE == 0)
 
-        #body creation
-        self.body = [square(WINDOWWIDTH/2-i*SQR_SIZE, WINDOWHEIGHT/2, SQR_SIZE) for i in range(10)]
+        # body creation
+        self.body = [square(WINDOWWIDTH / 2 - i * SQR_SIZE, WINDOWHEIGHT / 2, SQR_SIZE) for i in range(10)]
 
-        #print([val.get_cord() for val in self.body])
+        # print([val.get_cord() for val in self.body])
 
         self.head = self.body[0].get_cord()
-        #movement 2d vector
-        self.direction = (1,0)
+        # movement 2d vector
+        self.direction = (1, 0)
 
     def __len__(self):
         return len(self.body)
@@ -58,7 +71,7 @@ class snek:
     def set_direction(self, vec):
         assert (-1 <= vec[0] <= 1 and -1 <= vec[1] <= 1)
 
-        if not vec == tuple([i*(-1) for i in self.direction]):
+        if not vec == tuple([i * (-1) for i in self.direction]):
             self.direction = vec
 
     def get_direction(self):
@@ -67,25 +80,33 @@ class snek:
     def update_pos(self):
         """updates snek position, if direction change is provided (as a vector (dx, dy)) it changes the dir"""
 
-        #print(self.body[0].get_cord())
+        # print(self.body[0].get_cord())
         print(self.body)
 
-        last_node=None
+        last_node = None
 
-        #TODO: cos nie tak z przenoszeniem w lewo
-
+        # change position of all snek nodes
         for node in self.body:
             if node == self.body[0]:
-                node.move_by_vector(self.direction, MOVEMENT_STEP)
-                if node.get_cord()[0]> WINDOWWIDTH:
-                    node.set_cord([0, node.get_cord()[1]])
-                elif node.get_cord()[0] <0:
-                    node.set_cord([WINDOWWIDTH, node.get_cord()[1]])
+                node.move_by_vector(self.direction, MOVEMENT_STEP)  # move head by vector direction
+
+                # handle snek head out of window bounds
+                # x axis
+                if node.get_cord()[0] >= WINDOWWIDTH:
+                    node.set_cord([0, node.get_cord()[1]], False)
+                elif node.get_cord()[0] < 0:
+                    node.set_cord([WINDOWWIDTH - SQR_SIZE, node.get_cord()[1]], False)
+
+                # y axis
+                if node.get_cord()[1] >= WINDOWHEIGHT:
+                    node.set_cord([node.get_cord()[0], 0], False)
+                elif node.get_cord()[1] < 0:
+                    node.set_cord([node.get_cord()[0], WINDOWHEIGHT-SQR_SIZE], False)
 
             else:
                 node.set_cord(last_node.last_pos)
-            last_node = node
 
+            last_node = node  # save position of the last node
 
     def get_snek(self):
         return self.body
@@ -97,7 +118,6 @@ class App:
         self.screen = None
         self.snek = None
 
-
     def on_start(self):
         pygame.init()
         self.screen = pygame.display.set_mode(self.size)
@@ -106,14 +126,14 @@ class App:
         self.food_is = False
         self.snek = snek()
 
-
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self.run = False
-    def on_loop(self):
-        self.screen.fill((0,0,0))
 
-        #print(self.p1.get_cord())
+    def on_loop(self):
+        self.screen.fill((0, 0, 0))
+
+        # print(self.p1.get_cord())
         # mechanics
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]: self.snek.set_direction((0, -1))
@@ -127,21 +147,25 @@ class App:
             pygame.draw.rect(self.screen, WHITE, node.gett())
 
         CLOCK.tick(FPS)
+
     def on_render(self):
         pygame.display.flip()
+
     def on_cleanup(self):
         pygame.quit()
+
     def on_run(self):
-        if self.on_start()==False:
+        if self.on_start() == False:
             self.run = False
 
-        #runloop
+        # runloop
         while self.run:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
         self.on_cleanup()
+
 
 if __name__ == "__main__":
     a1 = App()
